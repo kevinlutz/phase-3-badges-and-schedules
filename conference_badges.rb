9d87fba233c043142c5168105ecaaaaaf87be72f
+ <+>
2021-10-13 22:11:25 -0400

    commit tree ca4c9a90780eef991bd8d27d824525c12a80b767

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map do |x|
+        badge_maker(x)
+    end
+end
